d0885ff226d6bb0690be86976f4e3935928827d7
Shanil Puri <spuri3@ncsu.edu>
2014-10-21 19:54:19 -0400

    Added header to for constant use.

--- components/compositing/constellation.rs
+++ components/compositing/constellation.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use devtools_traits;
 use compositor_task::{CompositorChan, LoadComplete, ShutdownComplete, SetLayerOrigin, SetIds};
 use devtools_traits::DevtoolsControlChan;
 use std::collections::hashmap::{HashMap, HashSet};
