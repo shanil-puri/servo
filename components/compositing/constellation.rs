1e835082aad9a017cf5c035f4b7c3a4fb0f9db1d
Shanil Puri <spuri3@ncsu.edu>
2014-10-23 22:31:14 -0400

    Moving new import next to old import for devtools_traits as specified in comment.

--- components/compositing/constellation.rs
+++ components/compositing/constellation.rs
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use devtools_traits;
 use compositor_task::{CompositorChan, LoadComplete, ShutdownComplete, SetLayerOrigin, SetIds};
+use devtools_traits;
 use devtools_traits::DevtoolsControlChan;
 use std::collections::hashmap::{HashMap, HashSet};
 use geom::rect::{Rect, TypedRect};
